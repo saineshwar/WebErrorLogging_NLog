d00ff5e995b982bc3b1118367625fbf66cc344ab
saineshwar bageri <saihacksoft@gmail.com>
2019-02-23 00:17:38 +0530

    First Commit
    First Commit

---
+++ WebErrorLogging/WebErrorLogging/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+
+
+namespace WebErrorLogging.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        // GET: Default
+        public ActionResult Index()
+        {
+            try
+            {
+                Logger.Debug("Hi I am NLog Debug Level");
+                Logger.Info("Hi I am NLog Info Level");
+                Logger.Warn("Hi I am NLog Warn Level");
+                throw new NullReferenceException();
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Hi I am NLog Error Level");
+                Logger.Fatal(ex, "Hi I am NLog Fatal Level");
+                throw;
+            }
+        }
+    }
+}
---
+++ WebErrorLogging/WebErrorLogging/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebErrorLogging.MvcApplication" Language="C#" %>
